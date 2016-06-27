a9d841d875d0a147e15ed1948370e19e196d90ad
Duoc Nguyen <nguyenvanduocit@gmail.com>
2016-06-27 12:09:05 +0700

    fix flag.Parse

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	var text string
 	flag.StringVar(&text, "text", "I love cat", "Text used to find emoji")
+	flag.Parse()
 	emojis, err := emoji.FindEmoji(text)
 	if err != nil {
 		fmt.Println(err)
